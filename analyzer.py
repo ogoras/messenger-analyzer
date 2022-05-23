17ef6958003c3c407e30504c950693be6b5b5617
ogoras <skus30@wp.pl>
2022-05-24 00:41:52 +0200

    characteristic words, still WIP

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,8 @@
 import os
 #import JSON library
 import json
+
+from numpy import size
 from loader import read_folder
 
 master_folder = os.path.join("messages", "inbox")
@@ -10,6 +12,7 @@ def print_fb(string):
 
 # create an empty dict
 all_vocabularies = {}
+total_words = {}
 
 for conversation_folder in os.listdir(master_folder):
     conversation_folder = os.path.join(master_folder, conversation_folder)
@@ -30,28 +33,70 @@ for conversation_folder in os.listdir(master_folder):
                         vocabulary[word] += 1
                     else:
                         vocabulary[word] = 1
+                    if sender in total_words:
+                        total_words[sender] += 1
+                    else:
+                        total_words[sender] = 1
 
-def print_vocab(vocabulary, n=100):
+def print_vocab(vocabulary, n=100, overall_vocabulary=None):
     print("Vocabulary size:" + str(len(vocabulary)))
     i = 0
     for word in sorted(vocabulary, key=vocabulary.get, reverse=True)[:n]:
         i += 1
-        print(str(i)+".", word, vocabulary[word])
+        if overall_vocabulary == None:
+            print(str(i)+".", word, vocabulary[word])
+        else:
+            print(str(i)+".", word, vocabulary[word], overall_vocabulary[word])
     
-for sender in all_vocabularies:
-    print(sender)
-    print_vocab(all_vocabularies[sender], 10)
-    print("")
-    vocabulary_sizes = {}
-    for sender in all_vocabularies.keys():
-        vocabulary_sizes[sender] = len(all_vocabularies[sender])
+def normalize_vocab(vocabulary):
+    sum = 0.0
+    for word in vocabulary:
+        sum += vocabulary[word]
+    print(sum)
+    normalized = {}
+    for word in vocabulary:
+        normalized[word] = vocabulary[word] * 100/sum
+    return normalized
+
+def relative_vocab(vocabulary, master_vocabulary):
+    relative_vocabulary = {}
+    for word in vocabulary:
+        relative_vocabulary[word] = vocabulary[word]/master_vocabulary[word]
+    return relative_vocabulary
+
+# for sender in all_vocabularies:
+#     print(sender)
+#     print_vocab(all_vocabularies[sender], 10)
+#     print("")
+
+vocabulary_sizes = {}
+for sender in all_vocabularies.keys():
+    vocabulary_sizes[sender] = len(all_vocabularies[sender])
 
 # print senders from largest vocabulary to smallest
 sorted_vocabularies = sorted(vocabulary_sizes, key=vocabulary_sizes.get, reverse=True)
 print("Sorted vocabularies:")
 i = 0
-for sender in sorted_vocabularies:
-    i += 1
-    print(str(i)+".", sender, vocabulary_sizes[sender])
+magnitude_order = float("inf")
 
-print ("Total vocabulary:", len(all_vocabularies.keys()))
+average_vocabulary = {}
+for sender in sorted_vocabularies:
+    if len(str(vocabulary_sizes[sender])) < magnitude_order:
+        magnitude_order = len(str(vocabulary_sizes[sender]))
+    i += 1
+    # print(str(i)+".", sender, vocabulary_sizes[sender])
+    for word in all_vocabularies[sender]:
+        if word in average_vocabulary:
+            average_vocabulary[word] += all_vocabularies[sender][word]/total_words[sender]*magnitude_order
+        else:
+            average_vocabulary[word] = all_vocabularies[sender][word]/total_words[sender]*magnitude_order
+
+average_vocabulary = normalize_vocab(average_vocabulary)
+
+for sender in ["your name"]:
+    print(sender)
+    print_vocab(relative_vocab(normalize_vocab(all_vocabularies[sender]), average_vocabulary), 100, all_vocabularies[sender])
+
+
+print("Average vocabulary:")
+print_vocab(average_vocabulary, 100)
