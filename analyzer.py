b30a963942ac767a17e234d5280e98aea58417da
ogoras <skus30@wp.pl>
2022-05-24 12:02:56 +0200

    moved vocabulary analysis to another file

--- analyzer.py
+++ analyzer.py
@@ -1,103 +1,29 @@
 import os
-#import JSON library
-import json
-import sys
 
-from numpy import size
 from loader import read_folder
-
-master_folder = os.path.join("messages", "inbox")
+from vocabulary import print_vocab, VocabularyAnalyzer
 
 def print_fb(string):
     print(string.encode('latin1').decode('utf8'))
 
-# create an empty dict
-all_vocabularies = {}
-total_words = {}
+if __name__ == "__main__":
+    master_folder = os.path.join("messages", "inbox")
 
-for conversation_folder in os.listdir(master_folder):
-    conversation_folder = os.path.join(master_folder, conversation_folder)
-    messages = read_folder(conversation_folder)
-    print_fb(messages[0]["title"])
-    for message_file in messages:
-        for message in message_file["messages"]:
-            if "content" in message and message["type"] == "Generic":
-                sender = message["sender_name"].encode('latin1').decode('utf8')
-                if sender not in all_vocabularies:
-                    all_vocabularies[sender] = {}
-                vocabulary = all_vocabularies[sender]
+    vocab_analyzer = VocabularyAnalyzer()
 
-                content = message["content"].encode('latin1').decode('utf8')
-                for word in content.split():
-                    word = word.lower()
-                    if word in vocabulary:
-                        vocabulary[word] += 1
-                    else:
-                        vocabulary[word] = 1
-                    if sender in total_words:
-                        total_words[sender] += 1
-                    else:
-                        total_words[sender] = 1
+    for conversation_folder in os.listdir(master_folder):
+        conversation_folder = os.path.join(master_folder, conversation_folder)
+        messages = read_folder(conversation_folder)
+        print_fb(messages[0]["title"])
+        for message_file in messages:
+            for message in message_file["messages"]:
+                vocab_analyzer.add_message_to_vocabulary(message)
 
-def print_vocab(vocabulary, n=100, overall_vocabulary=None):
-    print("Vocabulary size:" + str(len(vocabulary)))
-    i = 0
-    for word in sorted(vocabulary, key=vocabulary.get, reverse=True)[:n]:
-        i += 1
-        if overall_vocabulary == None:
-            print(str(i)+".", word, vocabulary[word])
-        else:
-            print(str(i)+".", word, vocabulary[word], overall_vocabulary[word])
-    
-def normalize_vocab(vocabulary, bias=0):
-    sum = 0.0
-    for word in vocabulary:
-        sum += vocabulary[word] + bias
+    vocab_analyzer.calculate_average_vocab()
 
-    normalized = {}
-    for word in vocabulary:
-        normalized[word] = (vocabulary[word] + bias) * 100/sum
-    return normalized
+    for sender in ["your name", "your friend's name"]:
+        print(sender)
+        print_vocab(vocab_analyzer.characteristic_vocab(sender), 100, vocab_analyzer.get_vocab(sender))
 
-def relative_vocab(vocabulary, master_vocabulary):
-    relative_vocabulary = {}
-    for word in vocabulary:
-        relative_vocabulary[word] = vocabulary[word]/ master_vocabulary[word]
-    return relative_vocabulary
-
-# for sender in all_vocabularies:
-#     print(sender)
-#     print_vocab(all_vocabularies[sender], 10)
-#     print("")
-
-vocabulary_sizes = {}
-for sender in all_vocabularies.keys():
-    vocabulary_sizes[sender] = len(all_vocabularies[sender])
-
-# print senders from largest vocabulary to smallest
-sorted_vocabularies = sorted(vocabulary_sizes, key=vocabulary_sizes.get, reverse=True)
-
-i = 0
-magnitude_order = float("inf")
-
-average_vocabulary = {}
-for sender in sorted_vocabularies:
-    if len(str(vocabulary_sizes[sender])) < magnitude_order:
-        magnitude_order = len(str(vocabulary_sizes[sender]))
-    i += 1
-    # print(str(i)+".", sender, vocabulary_sizes[sender])
-    for word in all_vocabularies[sender]:
-        if word in average_vocabulary:
-            average_vocabulary[word] += all_vocabularies[sender][word]/total_words[sender]*magnitude_order
-        else:
-            average_vocabulary[word] = all_vocabularies[sender][word]/total_words[sender]*magnitude_order
-
-average_vocabulary_normalized = normalize_vocab(average_vocabulary, 0.01)
-
-for sender in ["your name", "your friend's name"]:
-    print(sender)
-    print_vocab(relative_vocab(normalize_vocab(all_vocabularies[sender]), average_vocabulary_normalized), 100, all_vocabularies[sender])
-
-
-# print("Average vocabulary:")
-# print_vocab(average_vocabulary, 100)
+    print("Average vocabulary:")
+    print_vocab(vocab_analyzer.get_vocab(), 100)
---
+++ vocabulary.py
@@ -0,0 +1,77 @@
+def print_vocab(vocabulary, n=100, overall_vocabulary=None):
+    print("Vocabulary size:" + str(len(vocabulary)))
+    i = 0
+    for word in sorted(vocabulary, key=vocabulary.get, reverse=True)[:n]:
+        i += 1
+        if overall_vocabulary == None:
+            print(str(i)+".", word, vocabulary[word])
+        else:
+            print(str(i)+".", word, vocabulary[word], overall_vocabulary[word])
+
+def relative_vocab(vocabulary, master_vocabulary):
+    relative_vocabulary = {}
+    for word in vocabulary:
+        relative_vocabulary[word] = vocabulary[word]/ master_vocabulary[word]
+    return relative_vocabulary
+
+def normalize_vocab(vocabulary, bias=0):
+    sum = 0.0
+    for word in vocabulary:
+        sum += vocabulary[word] + bias
+
+    normalized = {}
+    for word in vocabulary:
+        normalized[word] = (vocabulary[word] + bias) * 100/sum
+    return normalized
+
+class VocabularyAnalyzer:
+    def __init__(self):    
+        self.all_vocabularies = {}
+        self.total_words = {}
+        self.average_vocabulary = {}
+        self.vocabulary_sizes = {}
+        self.average_vocabulary_normalized = None
+
+    def add_message_to_vocabulary(self, message):
+        if "content" in message and message["type"] == "Generic":
+            sender = message["sender_name"].encode('latin1').decode('utf8')
+            if sender not in self.all_vocabularies:
+                self.all_vocabularies[sender] = {}
+            vocabulary = self.all_vocabularies[sender]
+
+            content = message["content"].encode('latin1').decode('utf8')
+            for word in content.split():
+                word = word.lower()
+                if word in vocabulary:
+                    vocabulary[word] += 1
+                else:
+                    vocabulary[word] = 1
+                if sender in self.total_words:
+                    self.total_words[sender] += 1
+                else:
+                    self.total_words[sender] = 1
+
+    def calculate_average_vocab(self):
+        self.average_vocabulary = {}
+
+        for sender in self.all_vocabularies:
+            self.vocabulary_sizes[sender] = len(self.all_vocabularies[sender])
+
+        for sender in self.all_vocabularies:
+            magnitude_order = len(str(self.vocabulary_sizes[sender]))
+            for word in self.all_vocabularies[sender]:
+                if word in self.average_vocabulary:
+                    self.average_vocabulary[word] += self.all_vocabularies[sender][word]/self.total_words[sender]*magnitude_order
+                else:
+                    self.average_vocabulary[word] = self.all_vocabularies[sender][word]/self.total_words[sender]*magnitude_order
+        
+        self.average_vocabulary_normalized = normalize_vocab(self.average_vocabulary, 0.01)
+
+    def get_vocab(self, sender = None):
+        if sender == None:
+            return self.average_vocabulary
+        else:
+            return self.all_vocabularies[sender]
+
+    def characteristic_vocab(self, sender):
+        return relative_vocab(normalize_vocab(self.all_vocabularies[sender]), self.average_vocabulary_normalized)
