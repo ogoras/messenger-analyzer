572e3c7e1b26f2bf1646fa4eb5b95619e2ba33b4
ogoras <skus30@wp.pl>
2022-05-26 03:41:36 +0200

    some category filters

--- filtering/category_filter.py
+++ filtering/category_filter.py
@@ -1,5 +1,6 @@
-from filtering.filter import Filter, CompositeFilter
+from filtering.filter import Filter
 from abc import abstractmethod
+from lib.lexical_processing import match_words
 
 class CategoryFilter(Filter):
     def __init__(self, categorizer):
@@ -19,3 +20,29 @@ class EqualsFilter(CategoryFilter):
 
     def filter_category(self, category):
         return category == self.category
+
+class ContainsFilter(CategoryFilter):
+    def __init__(self, categorizer, categories):
+        super().__init__(categorizer)
+        self.categories = categories
+
+    def filter_category(self, category):
+        return category in self.categories
+
+class RangeFilter(CategoryFilter):
+    def __init__(self, categorizer, category_start, category_end):
+        super().__init__(categorizer)
+        self.category_start = category_start
+        self.category_end = category_end
+
+    def filter_category(self, category):
+        return self.category_start <= category <= self.category_end
+
+class MatchFilter(CategoryFilter):
+    def __init__(self, categorizer, pattern, match="whole"):
+        super().__init__(categorizer)
+        self.match = match
+        self.pattern = pattern
+
+    def filter_category(self, category):
+        return match_words(self.pattern, category)
